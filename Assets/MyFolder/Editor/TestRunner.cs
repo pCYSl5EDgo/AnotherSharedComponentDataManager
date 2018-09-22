6c6c07694c0e1354a650af2e1bc4fffddd98d62d
pCYSl5EDgo <pCYSl5EDgo@yahoo.co.jp>
2018-09-22 19:31:42 +0900

    [add]Editor拡張でTest RunnerをAlt+Shift+Fで呼べるようにした

---
+++ Assets/MyFolder/Editor/TestRunner.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public sealed class TestRunner : EditorWindow
+{
+    [MenuItem("Window/RunTestRunner &#f")]
+    public static void RunTestRunner()
+    {
+        EditorApplication.ExecuteMenuItem("Window/General/Test Runner");
+    }
+}
